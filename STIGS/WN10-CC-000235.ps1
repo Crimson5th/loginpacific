17da5b195808c3da7d08edda7d64f73e07fca686
Crimson5th <63945988+Crimson5th@users.noreply.github.com>
2025-05-03 14:22:02 -0400

    Create WN10-CC-000235.ps1

---
+++ STIGS/WN10-CC-000235.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the Windows Defender SmartScreen filter in Microsoft Edge provides warning messages and blocks potentially malicious websites and file downloads.
+
+.NOTES
+    Author          : Quintin Harrigin
+    LinkedIn        : linkedin.com/in/quintin-harrigin-21033b303/
+    GitHub          : github.com/Crimson5th
+    Date Created    : 2025-03-05
+    Last Modified   : 2025-03-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000235
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\(STIG-ID-WN10-CC-000235).ps1 
+#>
+
+# YOUR CODE GOES HERE
